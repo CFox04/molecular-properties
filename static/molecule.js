01806252383ff89a90be1d4ab1d01735ff82caff
Colton Fox <46362482+CFox04@users.noreply.github.com>
2022-07-08 13:31:18 -0400

    Add options panel

--- static/molecule.js
+++ static/molecule.js
@@ -1,15 +1,14 @@
 const moleculeWrapper = document.querySelector('.molecule-wrapper');
 const moleculeProperties = document.querySelector('.molecule-properties');
 const moleculeSVG = document.getElementById('molecule-svg');
-
-const FLOAT_PRECISION = 6;
+const precisionInput = document.getElementById('float-precision');
 
 function addMolecularPropertyElement(name, value) {
     let element = document.createElement('span');
     element.class = 'molecular-property';
 
     if (typeof value == "number" && !Number.isInteger(value)) {
-        value = value.toFixed(FLOAT_PRECISION);
+        value = value.toFixed(precisionInput.value);
     }
 
     element.innerHTML = `<small>${name}</small> ${value}`;
